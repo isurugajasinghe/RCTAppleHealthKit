3b2cfdf92b68716b7045b3228de6ad94f3fbcfa1
G.G. Isuru U. Gajasinghe <isuruudyoga@gmail.com>
2021-02-03 10:01:09 +0530

    Initial project setup

---
+++ AppleHealthKit/AppleHealthKit.h
@@ -0,0 +1,18 @@
+//
+//  AppleHealthKit.h
+//  AppleHealthKit
+//
+//  Created by Isuru Gajasinghe on 2021-02-03.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppleHealthKit.
+FOUNDATION_EXPORT double AppleHealthKitVersionNumber;
+
+//! Project version string for AppleHealthKit.
+FOUNDATION_EXPORT const unsigned char AppleHealthKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppleHealthKit/PublicHeader.h>
+
+
